4a22f5aba68c2eb006319f2b9aabca564c8aa7fb
uriel.braga@enacom.com.br <uriel.braga@enacom.com.br>
2018-10-29 10:32:07 -0300

    Primeiro commit

---
+++ Run.py
@@ -0,0 +1,17 @@
+def funcao (a,b):
+	print("A soma resulta em ");
+	print(a+b);
+
+
+
+
+
+
+
+
+a=input("Digite um valor para A: ");
+a=int(a);
+b=input("Digite um valor para B: ");
+b=int(b);
+
+funcao(a,b);
